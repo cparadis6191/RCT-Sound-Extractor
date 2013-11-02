3000e12db54fa37cd44f0865e2bebbc69f501a8a
Chad M. Paradis <cparadis6191@gmail.com>
2013-11-01 22:06:21 -0400

    Changed function calls to reflect changes in RCTSE.h
    Some functions return struct pointers now.
    
    Code now properly uses data in wav_header structs

--- RCTSE/src/RCTSE.c
+++ RCTSE/src/RCTSE.c
@@ -7,18 +7,21 @@
 int main(void) {
 	FILE *fp = fopen("../Css1.dat", "r");
 	FILE *out = fopen("test.wav", "w");
-	uint16_t *data = NULL;
-	struct css1_header c_header;
-	struct wav_header w_header;
+	uint16_t *data;
+	struct css1_header *c_header;
+	struct wav_header *w_header;
+	size_t data_size;
 
 	if (fp == NULL) {
 		fputs("File error", stderr);
 		exit(1);
 	}
 
-	parse_header(fp, &c_header);
-	parse_wav_header(fp, &w_header);
-	data = parse_wav_data(fp, &c_header);
+	c_header = parse_header(fp);
+	w_header = parse_wav_header(fp);
+
+	data_size = c_header->OffsetList[1] - c_header->OffsetList[0];
+	data = parse_wav_data(fp, c_header->OffsetList[0], c_header->OffsetList[1]);
 
 
 	if (out == NULL) {
@@ -26,9 +29,11 @@ int main(void) {
 		exit(1);
 	}
 
-	put_wav_header(out, &w_header);
-	put_wav_data(out, data, 2000);
+	put_wav_header(out, w_header, data_size);
+	put_wav_data(out, data, c_header->OffsetList[0], c_header->OffsetList[1]);
 
+	free(c_header->OffsetList);
+	free(data);
 	fclose(out);
 	fclose(fp);
 
@@ -37,94 +42,99 @@ int main(void) {
 }
 
 
-int parse_header(FILE *fp, struct css1_header *header) {
+struct css1_header* parse_header(FILE *fp) {
+	struct css1_header* c_header = malloc(sizeof(struct css1_header));
 	// Parse NumberOfSounds
-	header->NumberOfSounds = fgetDWORD(fp);
+	c_header->NumberOfSounds = fgetDWORD(fp);
 	// TODO
-	printf("NumberOfSounds: %i\n", header->NumberOfSounds);
+	printf("NumberOfSounds: %i\n", c_header->NumberOfSounds);
 
 	// Populate OffsetList
-	header->OffsetList = calloc(header->NumberOfSounds, sizeof(uint32_t));
+	c_header->OffsetList = calloc(c_header->NumberOfSounds, sizeof(uint32_t));
 
-	for (int i = 0; i < header->NumberOfSounds; i++) {
-		header->OffsetList[i] = fgetDWORD(fp);
+	for (unsigned int i = 0; i < c_header->NumberOfSounds; i++) {
+		c_header->OffsetList[i] = fgetDWORD(fp);
 
 		if (i == 0) {
-			printf("Offset %i: %i\n", i, header->OffsetList[i] - header->OffsetList[i]);
+			printf("Offset %i: %i\n", i, c_header->OffsetList[i] - c_header->OffsetList[i]);
 		} else {
-			printf("Offset %i: %i\n", i, header->OffsetList[i] - header->OffsetList[i - 1]);
+			printf("Offset %i: %i\n", i, c_header->OffsetList[i] - c_header->OffsetList[i - 1]);
 		}
 	}
 
 	// Parse DataSize
-	header->DataSize = fgetDWORD(fp);
+	c_header->DataSize = fgetDWORD(fp);
 
 	// TODO
-	printf("DataSize: %i\n", header->DataSize);
+	printf("DataSize: %i\n", c_header->DataSize);
 
 
-	return 0;
+	return c_header;
 }
 
-int parse_wav_header(FILE *fp, struct wav_header *header) {
+struct wav_header* parse_wav_header(FILE *fp) {
+	struct wav_header* w_header = malloc(sizeof(struct wav_header));
+
 	// Populate the wav_header struct
-	header->wFormatTag = fgetWORD(fp);
-	printf("%i\n", header->wFormatTag);
-	header->wChannels = fgetWORD(fp);
-	printf("%i\n", header->wChannels);
-	header->dwSamplesPerSec = fgetDWORD(fp);
-	printf("%i\n", header->dwSamplesPerSec);
-	header->dwAvgBytesPerSec = fgetDWORD(fp);
-	printf("%i\n", header->dwAvgBytesPerSec);
-	header->wBlockAlign = fgetWORD(fp);
-	printf("%i\n", header->wBlockAlign);
-	header->wBitsPerSample = fgetWORD(fp);
-	printf("%i\n", header->wBitsPerSample);
+	w_header->wFormatTag = fgetWORD(fp);
+	printf("%i\n", w_header->wFormatTag);
+	w_header->wChannels = fgetWORD(fp);
+	printf("%i\n", w_header->wChannels);
+	w_header->dwSamplesPerSec = fgetDWORD(fp);
+	printf("%i\n", w_header->dwSamplesPerSec);
+	w_header->dwAvgBytesPerSec = fgetDWORD(fp);
+	printf("%i\n", w_header->dwAvgBytesPerSec);
+	w_header->wBlockAlign = fgetWORD(fp);
+	printf("%i\n", w_header->wBlockAlign);
+	w_header->wBitsPerSample = fgetWORD(fp);
+	printf("%i\n", w_header->wBitsPerSample);
 
 
-	return 0;
+	return w_header;
 }
 
 // TODO get a single song first
-uint16_t* parse_wav_data(FILE *fp, struct css1_header *header) {
-	// Start back at the beginning
+uint16_t* parse_wav_data(FILE *fp, size_t offset_start, size_t offset_end) {
 	// fp should be at the first bit of actual data
 	// 16-bit samples
-	uint16_t *buffer = malloc((header->OffsetList[1] - header->OffsetList[0])*sizeof(uint16_t));
-	fread(buffer, sizeof(uint16_t), (header->OffsetList[1] - header->OffsetList[0]), fp);
+
+	// Move fp to first word of data
+	fseek(fp, offset_start, SEEK_SET);
+	uint16_t *buffer = malloc((offset_end - offset_start)*sizeof(uint16_t));
+	fread(buffer, sizeof(uint16_t), (offset_end - offset_start), fp);
 
 
 	return buffer;
 }
 
-int put_wav_header(FILE *fp, struct wav_header *header) {
+int put_wav_header(FILE *fp, struct wav_header *w_header, size_t data_size) {
 	// RIFF chunk descriptor
 	fputs("RIFF", fp);
-	fputWORD(16, fp); // TODO Size 36 + SubChunk2Size
-	fputs("WAVE", fp); // format
+	fputDWORD(2*data_size + 31, fp);	// TODO Size 36 + SubChunk2Size
+	fputs("WAVE", fp);	// format
 
 	// fmt sub-chunk
-	fputs("fmt", fp);
-	fputWORD(16, fp); // 16 for pcm, no compression
-	fputWORD(1, fp); // 1 for pcm
-	fputWORD(1, fp); // mono
-	fputDWORD(44100, fp); // byte rate // TODO maybe mixed
-	fputDWORD(22050, fp); // sample rate
-	fputWORD(2, fp); // block align
-	fputWORD(16, fp); // bits per sample
+	fputs("fmt", fp), fputc('\0', fp);			// Format chunk marker including null
+	fputWORD(16, fp);							// Size of format data
+	fputWORD(w_header->wFormatTag, fp);			// Type of format
+	fputWORD(w_header->wChannels, fp);			// Mono/stereo
+	fputDWORD(w_header->dwSamplesPerSec, fp);	// Sample rate
+	fputDWORD(w_header->dwAvgBytesPerSec, fp);	// Byte rate
+	fputWORD(w_header->wBlockAlign, fp);		// Block align
+	fputWORD(w_header->wBitsPerSample, fp);		// Bits per sample
 
-	// data sub-chunk
-	fputs("data", fp);
-	fputWORD(0, fp); // number of data bytes
+	// Data sub-chunk
+	fputs("data", fp);			// Data description header
+	fputDWORD(2*data_size, fp);	// Size of data bytes
 
-	// data
+	// Data
 
 
 	return 0;
 }
 
-int put_wav_data(FILE *fp, uint16_t *data, int size) {
-	fwrite(data, sizeof(uint16_t), size, fp);
+int put_wav_data(FILE *fp, uint16_t *data, size_t offset_start, size_t offset_end) {
+	fwrite(data, sizeof(uint16_t), offset_end - offset_start, fp);
 
 
 	return 0;
